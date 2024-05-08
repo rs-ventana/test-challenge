cecb2fb9da386f60c3fe1804fc16badd69778ff0
rs-ventana <ruac.rs@outlook.com>
2024-05-08 00:48:25 -0600

    ultima parte

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -4,6 +4,17 @@ import { getUser, getUsers } from "../users";
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
+describe("getUser", () => {
+    it("Should return the correct ID", async () => {
+      const user = await getUser(3);
+      expect(user.id).toBe(3); // revisar el numero 3
+    });
+  
+    it("Should return an message for a invalid ID. ", async () => {
+      await expect(getUser(5)).rejects.toThrow("User not found");
+    }); // verificar con un dato que no existe
+  });
 
 // READ documentation
 
+ 
--- src/users.js
+++ src/users.js
@@ -1,6 +1,6 @@
 // Imports small array of users
 // Pretend it's an API request
-const users = require("./data.js").default;
+const users = require("./datos/data.js").default;
 
 // Fetches all users
 const getUsers = () => {
@@ -9,16 +9,15 @@ const getUsers = () => {
 
 // Filters users by specific ID
 const getUser = (id) => {
-  return users.find((user) => user.id === id);
-};
-
-//ahora, fetch
-
-
-
-
-// test
-// console.log(getUser(3));
+  return new Promise ((resolve, reject) => { // dos resultados
+    process.nextTick(() => {
+      const user=users.find((user) => user.id === id );
+      user ? resolve(user) : reject(new Error("User not found")); // comprobar si el usuario existe
+      });
+  }):
+  };
 
 module.exports = { getUsers, getUser };
+
+// funcionó con error
 
